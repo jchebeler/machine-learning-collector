d5b80b5dd4ea13f219efe27a6631b970b5d0771a
coolwhip5 <jchebeler@gmail.com>
2018-12-04 23:54:30 -0500

    Refactoring to allow for unchecking selections

--- src/App.js
+++ src/App.js
@@ -16,11 +16,9 @@ class App extends Component {
 		}
 	];
 	this.state = {
-		ind:[],
-		dep:[],
+		indResponses:[],
+		depResponses:[],
 	}
-	this.indResponses = [];
-	this.depResponses = [];
   }
   
   tableRows = () => {
@@ -29,11 +27,11 @@ class App extends Component {
         <tr key={key}>
 			<td className={'question'}> {`${key + 1}) ${row.question}`} </td>
 			<td > 
-				<input type="radio" value="inp" name={`question_${key}`} 
+				<input type="checkbox" value="inp" name={`question_${key}`} checked={this.state.indResponses.indexOf(key) > -1} 
 					onChange={(e) => this.handleChange(e, key, 'inp')} />
 			</td>
 			<td > 
-				<input type="radio" value="dep"  name={`question_${key}`} 
+				<input type="checkbox" value="dep"  name={`question_${key}`} checked={this.state.depResponses.indexOf(key) > -1} 
 					onChange={(e) => this.handleChange(e, key, 'dep')} />
 			</td>
 		</tr>
@@ -53,32 +51,62 @@ class App extends Component {
 	);
   };
   
-  handleChange = (e, index, type) => {
+  handleChange = (e, value, type) => {
+	  let joined = [];
+	  let removed = [];
 	  if(type === 'inp'){
-		this.indResponses.push(index);
-		this.checkForRemoval(index, this.depResponses);
+		//if it doesnt exist in the array, its a new selection, add it and remove it from the other array
+		if(this.state.indResponses.indexOf(value) === -1) {
+			joined = this.state.indResponses.concat(value);
+			removed = this.checkForRemoval(value, this.state.depResponses);
+			this.setState({
+				indResponses: joined,
+				depResponses: removed,
+			});
+		}
+		//if it does exist in the array, its a remove selection,remove it from this array
+		else{
+			removed = this.checkForRemoval(value, this.state.indResponses);
+			this.setState({
+				indResponses: removed,
+			});
+		}
+		
 	  }
 	  else if(type === 'dep'){
-		this.depResponses.push(index);
-		this.checkForRemoval(index, this.indResponses);
+		//if it doesnt exist in the array, its a new selection, add it and remove it from the other array
+		if(this.state.depResponses.indexOf(value) === -1) {
+			joined = this.state.depResponses.concat(value);
+			removed = this.checkForRemoval(value, this.state.indResponses);
+			this.setState({
+				depResponses: joined,
+				indResponses: removed,
+			});
+		}
+		//if it does exist in the array, its a remove selection,remove it from this array
+		else{
+			removed = this.checkForRemoval(value, this.state.depResponses);
+			this.setState({
+				depResponses: removed,
+			});
+		};
 	  }
   };
   
   checkForRemoval = (value, array) => {
 	const finder = array.indexOf(value);
-	if(finder > -1) { 
+	if(finder > -1) {
 		array.splice(finder,1);
 	}
+	return array;
   };
+  
   handleSubmit = () => {
 	  const allowSubmission = this.verifyData();
 	  if(allowSubmission) {
-		  this.indResponses.sort();
-		  this.depResponses.sort();
-		  this.setState({
-			ind: this.indResponses,
-			dep: this.depResponses,
-		  });
+		  //this.state.indResponses.sort();
+		  //this.state.depResponses.sort();
+		  alert('Congrats, your data has been submitted.');
 	  }
 	  else {
 		  alert('Bad Input: You must select at least 1 independent variable and 1 dependent variable, or 2 independent variables to continue. ');
@@ -86,7 +114,7 @@ class App extends Component {
   };
   
   verifyData = () => {
-	  if(this.indResponses.length >= 2 || (this.depResponses.length >= 1 && this.indResponses >= 1)){
+	  if(this.state.indResponses.length >= 2 || (this.state.depResponses.length >= 1 && this.state.indResponses >= 1)){
 		  return true;
 	  }
 	  return false;
@@ -105,8 +133,8 @@ class App extends Component {
 				</tbody>
 			</table>
 			<div>
-				<div>{`Independent Results: ${this.state.ind.length > 0 ? this.state.ind : 'N/A'}`}</div> 
-				<div>{`Dependent Results: ${this.state.dep.length > 0 ? this.state.dep : 'N/A'}`}</div>
+				<div>{`Independent Results: ${this.state.indResponses.length > 0 ? this.state.indResponses : 'N/A'}`}</div> 
+				<div>{`Dependent Results: ${this.state.depResponses.length > 0 ? this.state.depResponses : 'N/A'}`}</div>
 			</div>
 		</div>
     );
